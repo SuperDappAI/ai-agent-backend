101d2ef324df4ebc24291ca639cbf3d4254ce888
EE127F <andrekmbf@protonmail.com>
2023-06-19 17:53:16 +0200

    exposing memory manager methods

--- main.py
+++ main.py
@@ -3,21 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from dotenv import load_dotenv
 import os
+import pinecone
 
-from agent import call_agent
-from curie_async import call_curie_async 
-from agent_async import call_async_agent 
-from agent_async2 import call_async_agent2 
-
-
-from langchain.chat_models import ChatOpenAI 
+from langchain.chat_models import ChatOpenAI
 from langchain import OpenAI
+from memory import MemoryManager
 
 import asyncio
 
 load_dotenv()
-os.getenv('OPENAI_API_KEY')
-os.getenv('SERPER_API_KEY')
+os.getenv("OPENAI_API_KEY")
+os.getenv("SERPER_API_KEY")
 
 origins = [
     "http://localhost",
@@ -28,43 +24,75 @@ origins = [
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# run with uvicorn main:app --reload
+
+
+@app.post("/query/")
+async def callAgent(query: str = Form(...)):
+    gptAnswer, cb = instance_agent_with_memory(query)
+    result = {"query": query, "answer": gptAnswer, "OpenAI_callback": cb}
+    return result
+
+
+# @app.post('/query_async_agent/')
+# async def callAsyncAgent(query: str = Form(...)):
+#     gptAnswer,cb = await call_async_agent(query)
+#     result = {'answer': gptAnswer, 'OpenAI_callback': cb}
+#     return result
+
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+
+from dotenv import load_dotenv
+import os
+
+from langchain.chat_models import ChatOpenAI 
+from langchain import OpenAI
+
+import asyncio
+
+
+load_dotenv()
+os.getenv('OPENAI_API_KEY')
+# os.getenv('SERPER_API_KEY')
+
+pinecone.init()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+    "http://localhost:8000",
+]
+
+app = FastAPI()
+
+# figure out middleware later
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # run with uvicorn main:app --reload
 
-@app.post('/query/')
-async def callAgent(query: str = Form(...)):
-    gptAnswer,cb = call_agent(query)
-    result = {'query': query, 'answer': gptAnswer, 'OpenAI_callback': cb}
+
+@app.post('/push_memory/')
+async def writeMemoryForUser(message: str = Form(...), llm_response: str = Form(...), user_id: str = Form(...)):
+    memory_manager = MemoryManager(user_id)
+    elapsed_time = memory_manager.push_memory(message,llm_response)
+    return {'elapsed_time': elapsed_time} 
+
+@app.post('/pull_memory/')
+async def pullRelevantMemoriesForUser(query: str = Form(...),user_id: str = Form(...)):
+    memory_manager = MemoryManager(user_id)
+    memories,elapsed_time = memory_manager.get_relevant_memories(query) 
+    result = {'chat_history': memories, 'elapsed_time': elapsed_time}
     return result
 
-@app.post('/query_async_agent/')
-async def callAsyncAgent(query: str = Form(...)):
-    gptAnswer,cb = await call_async_agent(query)
-    result = {'answer': gptAnswer, 'OpenAI_callback': cb}
-    return result
-
-@app.post('/query_curie/')
-async def call_async_curie(query: str = Form(...)):
-    result = await call_curie_async(query)
-    return result 
-
-@app.post('/query_async_agent2/')
-async def callAsyncAgent(query: str = Form(...)):
-    response,cb = await call_async_agent2(query)
-    gptAnswer = response
-    result = {'answer': gptAnswer, 'OpenAI_callback': cb}
-    return result
-
-@app.post('/query_async_agent_with_memory/')
-async def callAsyncAgent(query: str = Form(...), username: str = Form(...)):
-    response,cb = await call_async_agent_with_memory(query, username)
-    gptAnswer = response
-    result = {'answer': gptAnswer, 'OpenAI_callback': cb}
-    return result
+@app.post('/clear_user_memory/')
+async def clearUserMemory(user_id: str = Form(...)):
+    memory_manager = MemoryManager(user_id)
+    return memory_manager.clear_user_memory()
