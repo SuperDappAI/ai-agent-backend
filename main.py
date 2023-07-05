db2d7ea07197ab6dfd5aba143ba36e02f975bcbb
EE127F <andrekmbf@protonmail.com>
2023-07-05 12:02:00 -0400

    added test GET method, added logs

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from langchain import OpenAI
 from memory_search import MemoryManager
 
 from aiohttp import ClientSession
-import asyncio
+import logging
+# import asyncio
 # from streamingFunctionsAgent import streamingFunctionsAgent
 # from sse_starlette import EventSourceResponse
 
@@ -35,23 +36,39 @@ app = FastAPI()
 
 pinecone.init()
 
+LOGFILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app.log') 
+logging.basicConfig(filename=LOGFILE_PATH, filemode='a',format='%(name)s - %(message)s',force=True)
 
 @app.post('/push_memory/')
 async def writeMemoryForUser(message: str = Form(...), llm_response: str = Form(...), user_id: str = Form(...)):
+    logging.info(f'Writing memory for user {user_id}')
     memory_manager = MemoryManager(user_id)
     elapsed_time = memory_manager.push_memory(message, llm_response)
-    return {'elapsed_time': elapsed_time}
+    logging.info('Pushed memory for user %s, message: %s, response: %s', user_id, message, llm_response)  # log the data push
+    logging.info('Elapsed time for operation: %s', elapsed_time)  # log the elapsed time
 
+    return {'elapsed_time': elapsed_time}
 
 @app.post('/pull_memory/')
 async def pullRelevantMemoriesForUser(query: str = Form(...), user_id: str = Form(...), context: str = Form(...), k_num: int = Form(...), deepSearch: bool = Form(...)):
+    logging.info(f'Pulling relevant memories for user {user_id}')
     memory_manager = MemoryManager(user_id, k_num)
     memories, elapsed_time = memory_manager.get_relevant_memory_docs(
         query, deepSearch=deepSearch, context=context)
-    return memories, elapsed_time
+    logging.info('Pulled relevant memories for user %s, query: %s, context: %s', user_id, query, context)  # log the data pull
+    logging.info('Elapsed time for operation: %s', elapsed_time)  # log the elapsed time
 
+    return {'memories': memories, 'elapsed_time': elapsed_time}
 
 @app.post('/clear_user_memory/')
 async def clearUserMemory(user_id: str = Form(...)):
+    logging.info(f'Clearing user memory for user {user_id}')
     memory_manager = MemoryManager(user_id)
+    logging.info('Cleared user memory for user %s', user_id)
     return memory_manager.clear_user_memory()
+
+@app.get('/test_callback/')
+async def test_callback():
+    logging.info(f'Test callback')
+
+    return {'test': 'test'}
