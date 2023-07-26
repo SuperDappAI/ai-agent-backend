5b11e806fc115a73c405b08d43a8a6e2d1a1af1c
jagdeep sidhu <sidhujag@syscoin.org>
2023-07-25 17:35:41 -0700

    clean up

--- web_manager.py
+++ web_manager.py
@@ -1,29 +1,43 @@
-from dotenv import load_dotenv
 import time
 import shutil
-import schedule
+from dotenv import load_dotenv
 from llama_index import OpenAI, ServiceContext, LLMRerank, Document, VectorStoreIndex, StorageContext, load_index_from_storage
-
-load_dotenv()
-
+from pathlib import Path
+import schedule
+import threading
+import os
 
 class WebManager:
     def __init__(self):
+        load_dotenv()  # Load environment variables
+        os.getenv("OPENAI_API_KEY")  # Get API Key from environment variable
+
         self.dirpath = "./web"
-        self.index = []
-        self.query_engine = []
+        self.index = {}
+        self.query_engine = {}
         self.reranker = LLMRerank(choice_batch_size=5, top_n=3, service_context=ServiceContext.from_defaults(
             llm=OpenAI(temperature=0, model="gpt-3.5-turbo"),
         ))
         schedule.every(300).to(600).seconds.do(self.save)
+        
+        # Create new thread for schedule
+        with threading.Thread(target=self.run_continuously) as thread:
+            thread.start()
+
+    def run_continuously(self):
+        """Keep checking and running pending tasks every second."""
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
     def load(self, hash):
+        """Load existing index data from the filesystem for a specific hash."""
         print("WebManager: Loading from disk")
         start = time.time()
-        userpath = f"{self.dirpath}_{hash}"
-        if userpath.exists() and userpath.is_dir():
+        hashpath = Path(f"{self.dirpath}_{hash}")
+        if hashpath.exists() and hashpath.is_dir():
             # rebuild storage context
-            storage_context = StorageContext.from_defaults(persist_dir=userpath)
+            storage_context = StorageContext.from_defaults(persist_dir=hashpath)
 
             # load index
             self.index[hash] = load_index_from_storage(storage_context)
@@ -36,17 +50,19 @@ class WebManager:
         print(f"WebManager: Load operation took {end - start} seconds")
 
     def save(self):
+        """Persist current index data to the filesystem."""
         start = time.time()
         self.saving = True
-        for idx, doc in enumerate(self.index):
-            if doc.dirty is True:
-                doc.storage_context.persist(persist_dir=f"{self.dirpath}_{idx}")
-                doc.dirty = False
+        for hash_key in self.index:
+            if self.index[hash_key].dirty is True:
+                self.index[hash_key].storage_context.persist(persist_dir=f"{self.dirpath}_{hash_key}")
+                self.index[hash_key].dirty = False
         self.saving = False
         end = time.time()
         print(f"WebManager: Save operation took {end - start} seconds")
 
     def push_html(self, hash, urls, html_docs):
+        """Add new HTML data to the current index for a specific hash."""
         if hash in self.index:
             print("WebManager: Error push_html, hash already exists")
             return
@@ -65,6 +81,7 @@ class WebManager:
         print(f"WebManager: push_html operation took {end - start} seconds")
 
     def pull_html(self, hash, query):
+        """Fetch HTML data based on a query for a specific hash."""
         if hash not in self.query_engine:
             print("WebManager: Error pull_html, hash doesn't exists")
             return None
@@ -77,7 +94,9 @@ class WebManager:
         return response
 
     def delete_memory(self, hash):
-        hashpath = f"{self.dirpath}_{hash}"
+        """Delete all memories for a specific hash."""
+        hashpath = Path(f"{self.dirpath}_{hash}")
         if hashpath.exists() and hashpath.is_dir():
             shutil.rmtree(hashpath)
-        self.index[hash] = None
+        self.index.pop(hash, None)
+        self.query_engine.pop(hash, None)
