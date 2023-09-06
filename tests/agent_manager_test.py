44d303f01a7f0fb43ae76cc09c40548925dde4df
EE127F <andrekmbf@protonmail.com>
2023-09-06 14:30:47 -0400

    reworked agent manager test

--- tests/agent_manager_test.py
+++ tests/agent_manager_test.py
@@ -1,87 +1,103 @@
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, AsyncMock, patch
 from agent_manager import AgentManager, MemoryOutput, MemoryInput
-from qdrant_client import QdrantClient
-from langchain.llms import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from qdrant_retriever import QDrantVectorStoreRetriever
-from langchain.retrievers.document_compressors import CohereRerank
 from generative_memory import GenerativeAgentMemory
-from langchain.retrievers import ContextualCompressionRetriever
-from langchain.vectorstores import Qdrant
+from langchain.schema.language_model import BaseLanguageModel
+from langchain.schema import BaseRetriever
+from langchain.retrievers.document_compressors.base import BaseDocumentCompressor
 from memory_summarizer import MemorySummarizer
 from document_summarizer import FlexibleDocumentSummarizer
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import Document
-from datetime import datetime, timedelta
-from unittest.mock import AsyncMock
-from typing import Any, Dict
-import os
+from datetime import datetime
+from pydantic import BaseModel, Field
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
 
-class TestAgentManager:
-    @pytest.fixture(autouse=True)
-    def setup_teardown(self):
-        self.agent_manager = AgentManager()
-        yield
+class MockLanguageModel(BaseLanguageModel):
+    async def agenerate_prompt(self, *args, **kwargs):
+        pass
+    async def apredict(self, *args, **kwargs):
+        pass
+    async def apredict_messages(self, *args, **kwargs):
+        pass
+    def generate_prompt(self, *args, **kwargs):
+        pass
+    def invoke(self, *args, **kwargs):
+        pass
+    def predict(self, *args, **kwargs):
+        pass
+    def predict_messages(self, *args, **kwargs):
+        pass
 
-    @pytest.mark.asyncio
-    async def test_push_memory(self):
-        test_api_key = os.getenv("OPENAI_API_KEY")
-        memory_output = MemoryOutput(
-            api_key=test_api_key,
-            user_id="test_user_id",
-            query="test_query",
-            llm_response="test_llm_response",
-            conversation_id="test_conversation_id",
-            importance="high"
-        )
+    class Config:
+        arbitrary_types_allowed = True
 
-        with patch.object(self.agent_manager, 'load', return_value=MagicMock()) as mock_load:
-            result = await self.agent_manager.push_memory(memory_output)
+class MockBaseDocumentCompressor(BaseDocumentCompressor):
+    async def acompress_documents(self, *args, **kwargs):
+        pass
+    def compress_documents(self, *args, **kwargs):
+        pass
 
-        assert isinstance(result, float)
-        mock_load.assert_called_once_with(memory_output.api_key, memory_output.user_id)
+class MockVectorStore:
+    aadd_documents = AsyncMock()
 
-    def test_create_new_memory_retriever(self):
+class MockBaseRetriever(BaseRetriever):
+    vectorstore = MockVectorStore()
 
-        test_api_key = os.getenv("OPENAI_API_KEY")
-        with patch.object(self.agent_manager.client, 'create_collection') as mock_create_collection, \
-             patch.object(self.agent_manager.client, 'create_payload_index') as mock_create_payload_index:
-            result = self.agent_manager.create_new_memory_retriever(test_api_key, "test_user_id")
+    def _get_relevant_documents(self, *args, **kwargs):
+        pass
 
-        assert isinstance(result, ContextualCompressionRetriever)
-        mock_create_collection.assert_called_once()
-        mock_create_payload_index.assert_called_once()
+class MockMemoryRetriever(BaseModel):
+    base_compressor: MockBaseDocumentCompressor = Field(default_factory=MockBaseDocumentCompressor)
+    base_retriever: MockBaseRetriever = Field(default_factory=MockBaseRetriever)
 
-    # def test_create_memory(self):
-    #     test_api_key = os.getenv("OPENAI_API_KEY")
-    #     mock_retriever = MagicMock(base_compressor=MagicMock(), base_retriever=MagicMock())
-    #     with patch.object(self.agent_manager, 'create_new_memory_retriever', return_value=mock_retriever) as mock_create_new_memory_retriever:
-    #         with patch('generative_memory.GenerativeAgentMemory', return_value=MagicMock(memory_retriever=mock_retriever, llm=MagicMock(), memory_summarizer=MagicMock())) as mock_generative_agent_memory:
-    #             result = self.agent_manager.create_memory(test_api_key, "test_user_id")
+    class Config:
+        arbitrary_types_allowed = True
 
-    #     assert isinstance(result, MagicMock)
-    #     mock_create_new_memory_retriever.assert_called_once_with(test_api_key, "test_user_id")
+class MockFlexibleDocumentSummarizer(FlexibleDocumentSummarizer):
+    pass
 
-    # @pytest.mark.asyncio
-    # async def test_pull_memory(self):
-    #     test_api_key = os.getenv("OPENAI_API_KEY")
-    #     memory_input = MemoryInput(
-    #         api_key=test_api_key,
-    #         user_id="test_user_id",
-    #         query="test_query",
-    #         conversation_id="test_conversation_id",
-    #         summary=True
-    #     )
+class MockMemorySummarizer(MemorySummarizer):
+    pass
 
-    #     async def async_magic_mock(*args, **kwargs):
-    #         return {"relevant_summary": "test_summary"}, 0.1
+@pytest.fixture
+def setup_agent_manager():
+    agent_manager = AgentManager()
+    return agent_manager
 
-    #     with patch.object(self.agent_manager, 'load_summary', new_callable=AsyncMock, return_value=async_magic_mock()) as mock_load_summary, \
-    #          patch.object(self.agent_manager, 'load_memory') as mock_load_memory:
-    #         result, time_taken = await self.agent_manager.pull_memory(memory_input)
+@pytest.mark.asyncio
+async def test_push_memory(setup_agent_manager):
+    agent_manager = setup_agent_manager
+    memory_output = MemoryOutput(
+        api_key="test_api_key",
+        user_id="test_user_id",
+        query="test_query",
+        llm_response="test_llm_response",
+        conversation_id="test_conversation_id",
+        importance="high"
+    )
 
-    #     assert isinstance(result, dict)
-    #     assert isinstance(time_taken, float)
-    #     mock_load_summary.assert_called_once_with(memory_input)
-    #     mock_load_memory.assert_not_called()
+    with patch.object(agent_manager, 'load', return_value=GenerativeAgentMemory(
+        llm=MockLanguageModel(),
+        memory_retriever=MockMemoryRetriever(),
+        memory_summarizer=MockMemorySummarizer(
+            flexible_document_summarizer=MockFlexibleDocumentSummarizer(
+                llm=MockLanguageModel()
+            ),
+            agent_manager=setup_agent_manager
+        ),
+        verbose=True
+    )) as mock_load:
+        result = await agent_manager.push_memory(memory_output)
+
+    assert isinstance(result, float)
+    mock_load.assert_called_once_with(memory_output.api_key, memory_output.user_id)
+
+def test_create_new_memory_retriever(setup_agent_manager):
+    agent_manager = setup_agent_manager
+
+    with patch.object(agent_manager.client, 'create_collection') as mock_create_collection, \
+         patch.object(agent_manager.client, 'create_payload_index') as mock_create_payload_index:
+        result = agent_manager.create_new_memory_retriever("test_api_key", "test_user_id")
+
+    assert isinstance(result, ContextualCompressionRetriever)
+    mock_create_collection.assert_called_once()
+    mock_create_payload_index.assert_called_once()
